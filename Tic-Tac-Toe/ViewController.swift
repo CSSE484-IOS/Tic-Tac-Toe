744921c4ec4d78df409046c466a88c9e57f2957e
Yizhi Feng <fengy2@rose-hulman.edu>
2018-03-19 12:15:20 -0400

    completed app

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -10,13 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var gameBoardBtns: [UIButton]!
+    @IBOutlet weak var gameStateLabel: UILabel!
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    @IBOutlet weak var gameStateImageView: UIImageView!
+    
+    var game = TicTacToeGame()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gameStateNavBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 34)]
+        if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact {
+            print("Using iPhone board")
+            gameStateImageView.image = #imageLiteral(resourceName: "iPhone_board")
+        } else {
+            print("Using iPad board")
+            gameStateImageView.image = #imageLiteral(resourceName: "iPad_board")
+        }
+        updateView()
     }
     
     @IBAction func presssedNewGame(_ sender: Any) {
         print("You pressed new game")
+        game = TicTacToeGame()
+        updateView()
     }
+    
+    @IBAction func pressedGameBoardButton(_ sender: Any) {
+//        guard let gameBoardBtn = sender as? UIButton else {
+//            return;
+//        }
+        let gameBoardBtn = sender as! UIButton
+        print(gameBoardBtn.tag)
+        game.pressedSquare(gameBoardBtn.tag)
+        updateView()
+    }
+    
+    func updateView() {
+        var xImage: UIImage
+        var oImage: UIImage
+        
+        if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact {
+            gameStateLabel.text = game.gameState.rawValue
+            xImage = #imageLiteral(resourceName: "iPhone_X")
+            oImage = #imageLiteral(resourceName: "iPhone_O")
+        } else {
+            gameStateNavBar.topItem?.title = game.gameState.rawValue
+            xImage = #imageLiteral(resourceName: "iPad_X")
+            oImage = #imageLiteral(resourceName: "iPad_O")
+        }
+        
+        for i in 0..<9 {
+            let btn = gameBoardBtns[i]
+            switch game.gameBoard[i] {
+            case .x:
+                btn.setImage(xImage, for: UIControlState.normal)
+            case .o:
+                btn.setImage(oImage, for: UIControlState.normal)
+            case .none:
+                btn.setImage(nil, for: UIControlState.normal)
+            }
+        }
+    }
+    
 }
 
